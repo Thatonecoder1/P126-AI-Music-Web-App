ad120ed8c2a10bb8a396413f923a535ff48de565
Thatonecoder1 <83841772+Thatonecoder1@users.noreply.github.com>
2021-12-30 20:58:30 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+song = "";
+
+function preload(){
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+ var scoreRightWrist = 0,scoreLeftWrist = 0,rightWristX= 0,rightWristY = 0,leftWristX = 0,leftWristY = 0;
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+poseNet = ml5.poseNet(video,modelLoaded());
+poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1)
+}

671166976a5e093f084fdfe64f8e93c7846e8577
Thatonecoder1 <83841772+Thatonecoder1@users.noreply.github.com>
2022-01-05 20:46:44 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-song = "";
+song1 = "";
+song2 = "";
+
+song1_status = "";
+song2_status = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+leftWristX = 0;
+leftWristY = 0;
+rightWristY = 0;
+rightWristX = 0;
 
 function preload(){
     song1 = loadSound("music.mp3");
@@ -18,10 +29,57 @@ poseNet.on('pose',gotPoses);
 
 function draw(){
     image(video,0,0,600,500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+    
+  
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX,leftWristY,20);
+        song1.stop();
+        
+        if(song2_status == false){
+            song2.play();
+        
+        document.getElementById("song").innerHTML = " Playing - Peter Pan Song ";
+        }
+    } if(scoreRightWrist > 0.2){
+        circle(rightWristX,rightWristY,20);
+       
+        song2.stop();
+        
+        if(song1_status == false){
+            song1.play();
+        
+        document.getElementById("song").innerHTML = " Playing - Harry Potter Theme Song";
+        }       
+    }
 }
 
 function play(){
     song.play();
     song.setVolume(1);
     song.rate(1)
+}
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist = "+ scoreRightWrist + "scoreLeftWrist = " + scoreleftWrist);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "lefttWristY = "+ leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = "+ rightWristY);
+    }
 }
